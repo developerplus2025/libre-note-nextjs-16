0a395043fae44f6b6afbd65cb0ab5aee18409401
developerplus2025 <anpham08112009@gmail.com>
2025-10-26 10:18:34 +0700

    df

--- source.config.ts
+++ source.config.ts
@@ -1,24 +1,30 @@
-import { defineDocs } from "fumadocs-mdx/config";
-import { defineCollections, frontmatterSchema } from "fumadocs-mdx/config";
-import { array, z, ZodArray } from "zod";
+import {
+  defineDocs,
+  defineCollections,
+  frontmatterSchema,
+} from "fumadocs-mdx/config";
+import { z } from "zod";
+
 export const docs = defineDocs({
   dir: "content/docs",
 });
+
 export const blogPosts = defineCollections({
   type: "doc",
   dir: "content/blog",
-  // add required frontmatter properties
   schema: frontmatterSchema.extend({
     description: z.string().optional(),
-    authors: z.array(
-      z.object({
-        name: z.string(),
-        avatar: z.string(),
-        username: z.string(),
-      }),
-    ),
-    category: z.string(),
-    date: z.string(),
-    link: z.string(),
+    authors: z
+      .array(
+        z.object({
+          name: z.string(),
+          avatar: z.string(),
+          username: z.string(),
+        }),
+      )
+      .optional(), // 👈 thêm optional nếu không phải bài nào cũng có authors
+    category: z.string().optional(),
+    date: z.string().optional(),
+    link: z.string().optional(),
   }),
 });
