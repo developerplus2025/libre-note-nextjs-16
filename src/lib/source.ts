963b155857842bcf30c80876d3c3a33a147eeac5
developerplus2025 <anpham08112009@gmail.com>
2025-10-26 10:03:41 +0700

    cv

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,13 +1,12 @@
 import { docs } from "@/.source";
 import { loader } from "fumadocs-core/source";
 import { blogPosts } from "@/.source";
-import { createMDXSource } from "fumadocs-mdx";
-export const source: ReturnType<typeof loader> = loader({
+
+export const source = loader({
   baseUrl: "/docs",
   source: docs.toFumadocsSource(),
 });
-
 export const blog = loader({
   baseUrl: "/blog",
-  source: createMDXSource(blogPosts),
+  source: docs.toFumadocsSource(blogPosts),
 });
