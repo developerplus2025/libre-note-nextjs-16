4701a252854a17c2ca9271f45f04d522e190f8c5
developerplus2025 <anpham08112009@gmail.com>
2025-10-27 14:43:59 +0700

    Refactor Sidebar component to improve structure by moving ScrollArea inside aside

--- src/components/layout/docs.tsx
+++ src/components/layout/docs.tsx
@@ -90,17 +90,15 @@ function Sidebar() {
   }, [root]);
 
   return (
-    <ScrollArea className="md:h-[calc(100dvh-118px)]">
-      <aside
-        className={cn(
-          "fixed top-[5rem] z-20 flex shrink-0 flex-col overflow-auto p-4 text-sm md:sticky md:h-[calc(100dvh-118px)] md:w-[290px]",
-          "max-md:bg-fd-background max-md:inset-x-0 max-md:bottom-0",
-          !open && "max-md:invisible",
-        )}
-      >
-        {children}
-      </aside>
-    </ScrollArea>
+    <aside
+      className={cn(
+        "fixed top-[5rem] z-20 flex shrink-0 flex-col overflow-auto p-4 text-sm md:sticky md:h-[calc(100dvh-118px)] md:w-[290px]",
+        "max-md:bg-fd-background max-md:inset-x-0 max-md:bottom-0",
+        !open && "max-md:invisible",
+      )}
+    >
+      <ScrollArea className="md:h-[calc(100dvh-118px)]">{children}</ScrollArea>
+    </aside>
   );
 }
 
